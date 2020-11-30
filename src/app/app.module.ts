ff4a9b6df996d2b7362ecb43e7bb7719b1188e8a
YOVANY LEZCANO CANO <jhonatanlezcano.plv@gmail.com>
2020-11-30 15:48:21 -0500

    subiendo primeros archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
+import { APP_ROUTING } from './app.routes';
+
+
+// Servicios
+
+// Componenter
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import {  Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { AboutComponent } from './components/about/about.component';
+
+
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'heroes', component: HeroesComponent },
+    { path: 'about', component: AboutComponent },
+    { path: '**', pathMatch: 'full', redirectTo: 'home' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
